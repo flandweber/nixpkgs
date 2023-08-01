2d0690d424f2ae4c582f42fc6af21b4f24c2fdc8
Alyssa Ross <hi@alyssa.is>
2023-08-01 19:04:49 +0000

    aemu: init at 0.1.2
    (cherry picked from commit 9cce064d4aff9b587460654a0b05c8219b0732b7)

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -20472,6 +20472,8 @@ with pkgs;
 
   adslib = callPackage ../development/libraries/adslib { };
 
+  aemu = callPackage ../development/libraries/aemu { };
+
   afflib = callPackage ../development/libraries/afflib { };
 
   aften = callPackage ../development/libraries/aften { };
